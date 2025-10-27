a371a0ec759e724740be0eae62685a6656806928
유선영\catsp <catspie92@gmail.com>
2025-10-27 22:45:44 +0900

    기본 스프링 부트 셋팅

---
+++ src/main/java/com/example/loghome_project/LogHomeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.loghome_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogHomeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogHomeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loghome_project/LogHomeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loghome_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogHomeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
